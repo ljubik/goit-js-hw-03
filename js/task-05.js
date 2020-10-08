7e64baf679a2c6a05f70c2ad46ebcacc2815b64f
Liubomyr <ljubik@ukr.net>
2020-10-08 18:22:32 +0300

    make task#05 complete

--- js/task-05.js
+++ js/task-05.js
@@ -1,24 +1,59 @@
 // Завдання 5
-// Задача 2-5
-// Функция предикат
-// Функция предикат возвращает true или false
+// Задача 3-5
+// Оператор in и метод push
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства.
 
-// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
-// Функция проверяет ее на содержание слов spam и sale. Если нашли запрещенное слово то
-// функция возвращает true, если
-// запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
+// Функция возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+
+// Используй метод push для добавления значения в массив и оператор in для проверки
+// наличия свойства в объекте.
 
 // Здано:
-
-let checkForSpam = function (str) {
-	let strForCheck = str.toLowerCase()
-	return strForCheck.includes(`spam`) || strForCheck.includes(`sale`)
+function getAllPropValues(array, prop) {
+	"use strict"
+	// Write code under this line
+	
+	return array.reduce(function(result, item) {
+		if (typeof item[prop] != "undefined")   
+		 
+		  result.push(item[prop]);
+		return result;
+	  }, []);
+	}
 }
 
-console.log(checkForSpam("Latest technology news")) // false
+// Объекты и ожидаемый результат
+const products = [
+	{ name: "Радар", price: 1300, quantity: 4 },
+	{ name: "Радар", price: 1280, quantity: 2 },
+	{ name: "Радар", price: 1320, quantity: 1 },
+	{ name: "Сканер", price: 2700, quantity: 1 },
+	{ name: "Сканер", price: 2500, quantity: 3 },
+	{ name: "Дроид", price: 400, quantity: 7 },
+	{ name: "Захват", price: 1200, quantity: 2 },
+]
 
-console.log(checkForSpam("JavaScript weekly newsletter")) // false
+console.log(getAllPropValues(products, "name"))
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-console.log(checkForSpam("Get best sale offers now!")) // true
+console.log(getAllPropValues(products, "quantity"))
+// [4, 2, 1, 1, 3, 7, 2]
 
-console.log(checkForSpam("[SPAM] How to earn fast money?")) // true
+console.log(getAllPropValues(products, "category"))
+//  []
+
+// function getAllPropValues(array, prop) {
+// 	"use strict"
+// 	// Write code under this line
+// 	//return arr.map(el => el[prop]); //ше один варіант
+
+// 	return array.reduce(function (result, item) {
+// 		if (
+// 			result.indexOf(item[prop]) == -1 && // якщо потрібні унікальні значення
+// 			typeof item[prop] != "undefined"
+// 		)
+// 			// якщо не потрібно
+// 			result.push(item[prop])
+// 		return result
+// 	}, [])
+// }
